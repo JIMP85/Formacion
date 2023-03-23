a967ac1b5e7d84beb5f204ad80a614ec03daa9e2
JIMP85 <tharinol@gmail.com>
2023-03-23 22:36:14 +0100

    Control de excepciones

--- demo/src/main/java/com/example/ioc/StringRepositoryImpl.java
+++ demo/src/main/java/com/example/ioc/StringRepositoryImpl.java
@@ -1,25 +1,35 @@
 package com.example.ioc;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import com.example.exceptions.ArgumentoInvalidoException;
+import com.example.exceptions.InvalidDataException;
 
 @Repository
 @Primary
+@Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE) 
 public class StringRepositoryImpl implements StringRepository {
-
 	private String ultimo = "";
 	
 	@Override
 	public String load() {
-		// TODO Auto-generated method stub
-		return "Soy el StingRepositoryImpl";
+		return "Soy el StringRepositoryImpl";
 	}
 
 	@Override
-	public void save(String item) {
-		System.out.println("Guardo el item: " +item);
-
+	public void save(String item) throws InvalidDataException {
+		if(item == "")
+			throw new InvalidDataException("La cadena no puede estar vacia");
+		
+//		throw new ArgumentoInvalidoException();
+		System.out.println("Anterior: " + ultimo);
+		
+		this.ultimo = item;
+		System.out.println("Guardo el item: " + item);
 	}
 
 }
