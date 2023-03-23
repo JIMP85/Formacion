142452092a62132718eaa0a0a4d0cfdcf58de511
JIMP85 <tharinol@gmail.com>
2023-03-23 10:04:41 +0100

    Tests

--- demo/src/main/java/com/example/ejemplos/Calculadora.java
+++ demo/src/main/java/com/example/ejemplos/Calculadora.java
@@ -1,8 +1,25 @@
 package com.example.ejemplos;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Calculadora {
 
 	public double suma(double a, double b){
-		return a+b;
+		BigDecimal rslt = BigDecimal.valueOf(a+b);
+		return rslt.setScale(15,RoundingMode.HALF_DOWN).doubleValue();
 	}
+	
+	public double divide(double a, double b){
+		
+		if(b==0) throw new ArithmeticException("Divide entre 0");
+			
+		return a/b;
+	}
+	
+	public double divide(int a, int b){
+			
+		return a/b;
+	}
+
 }
--- demo/src/test/java/com/example/ejemplos/CalculadoraTest.java
+++ demo/src/test/java/com/example/ejemplos/CalculadoraTest.java
@@ -2,61 +2,112 @@ package com.example.ejemplos;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-import lombok.experimental.var;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("deprecation")
 class CalculadoraTest {
 
+	Calculadora calc;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		calc = new Calculadora();
+	}
+	
+	@Nested
+	@DisplayName("Pruebas del método Suma")
+	@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+	class Suma {
+		@Nested
+		class OK {
+			@Test
+			void test_Suma_Positivo_Positivo() {
+				var calc = new Calculadora();
+
+				var rslt = calc.suma(2, 2);
+
+				assertEquals(4, rslt);
+			}
+
+			@Test
+			void testSumaNegativoPositivo() {
+				var calc = new Calculadora();
+
+				var rslt = calc.suma(-1, 5);
+
+				assertEquals(4, rslt);
+			}
+			
+			@Test
+			void testSumaPositivoNegativo() {
+				var calc = new Calculadora();
+
+				var rslt = calc.suma(1, -6);
+
+				assertEquals(-5, rslt);
+			}
+
+			@Test
+			void testSumaNegativoNegativo() {
+				var calc = new Calculadora();
+
+				var rslt = calc.suma(-1, -5);
+
+				assertEquals(-6, rslt);
+			}
+
+			@Test
+			void testSumaDecimales() {
+				var calc = new Calculadora();
+
+				var rslt = calc.suma(0.2, 0.2);
+
+				assertEquals(0.4, rslt);
+			}
+
+		}
+
+		@Nested
+		class KO {
+
+		}
+
 	}
 
-	@Test
-	void testSuma() {
-		var calc = new Calculadora();
-		
-		var rslt = calc.suma(2, 2);
-		
-		assertEquals(4, rslt);
-	}
-	
-	@Test
-	void testSumaPositivoNegativo() { //Aconsejable probar diferentes valores
-		var calc = new Calculadora();
-		
-		var rslt = calc.suma(3, -1);
-		
-		assertEquals(2, rslt);
-	}
-	
-	@Test
-	void testSumaNegativoPositivo() {
-		var calc = new Calculadora();
-		
-		var rslt = calc.suma(-5, 7);
-		
-		assertEquals(2, rslt);
-	}
-	
-	@Test
-	void testSumaNegativoNegativo() {
-		var calc = new Calculadora();
-		
-		var rslt = calc.suma(-1, -4);
-		
-		assertEquals(-5, rslt);
-	}
-	
-	@Test
-	void testSumaDecimales() {  //Da error por la precisión del último bit del double
-		var calc = new Calculadora();
-		
-		var rslt = calc.suma(0.1, 0.2);
-		
-		assertEquals(0.3, rslt);
+	@Nested
+	class Divide {
+		@Nested
+		class OK {
+			@Test
+			void testDividirPorCero() {
+				var calc = new Calculadora();
+
+				var rslt = calc.divide(1, 0.0);
+				
+				assertEquals(1, rslt);
+
+				
+			}
+
+		}
+
+		@Nested
+		class KO {
+			@Test
+			void testDividirPorCero() {
+				var calc = new Calculadora();
+				
+				assertThrows(ArithmeticException.class, ()-> calc.divide(1, 0));
+			}
+			
+		}
+
 	}
 
 	
