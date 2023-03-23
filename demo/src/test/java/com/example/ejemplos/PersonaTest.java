94e5604ebabb9563232f6f203c0ebf2261691777
JIMP85 <tharinol@gmail.com>
2023-03-23 22:53:58 +0100

    Practica assert

--- demo/src/test/java/com/example/ejemplos/PersonaTest.java
+++ demo/src/test/java/com/example/ejemplos/PersonaTest.java
@@ -1,13 +1,13 @@
 package com.example.ejemplos;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 
 import com.example.core.test.Smoke;
-
 
 class PersonaTest {
 
@@ -16,32 +16,31 @@ class PersonaTest {
 	}
 
 	@Test
+	@Smoke
 	void testCreate() {
 		var p = Persona.builder().id(1).nombre("Pepito").apellidos("Grillo").build();
-//		p=null;
-		assertNotNull(p);
-		assertTrue(p instanceof Persona, "No es una instance de Persona");
+//		p = null;
+//		assertNotNull(p);
+		assertTrue(p instanceof Persona, "No es instancia de persona");
 		assertAll("Validar propiedades",
-		()-> assertEquals(1, p.getId(),"id"),
-		()-> assertEquals("Pepito", p.getNombre(),"getNombre"),
-		()-> assertEquals("Grillo", p.getApellidos(),"getApellido")
+			()-> assertEquals(1, p.getId(), "id"),
+			()-> assertEquals("Pepito", p.getNombre(), "getNombre"),
+			()-> assertEquals("Grillo", p.getApellidos(), "getApellidos")
 		);
-		
 	}
-	@Smoke
 	@RepeatedTest(value = 5, name = "{displayName} {currentRepetition}/{totalRepetitions}")
 	void repeatedTest(RepetitionInfo repetitionInfo) {
 		var p = Persona.builder()
 				.id(repetitionInfo.getCurrentRepetition())
-				.nombre("Pepito" + (repetitionInfo.getCurrentRepetition() % 3 == 0 ? "" : repetitionInfo.getCurrentRepetition()))
+				.nombre("Pepito" + repetitionInfo.getCurrentRepetition()) // (repetitionInfo.getCurrentRepetition() % 3 == 0 ? "" : repetitionInfo.getCurrentRepetition()))
 				.apellidos("Grillo").build();
 //		p = null;
 //		assertNotNull(p);
 		assertTrue(p instanceof Persona, "No es instancia de persona");
 		assertAll("Validar propiedades",
-			() -> assertEquals(repetitionInfo.getCurrentRepetition(), p.getId(), "id"),
-			() -> assertEquals("Pepito" + repetitionInfo.getCurrentRepetition(), p.getNombre(), "getNombre"),
-			() -> assertEquals("Grillo", p.getApellidos(), "getApellidos")
+			()-> assertEquals(repetitionInfo.getCurrentRepetition(), p.getId(), "id"),
+			()-> assertEquals("Pepito" + repetitionInfo.getCurrentRepetition(), p.getNombre(), "getNombre"),
+			()-> assertEquals("Grillo", p.getApellidos(), "getApellidos")
 		);
 	}
 
