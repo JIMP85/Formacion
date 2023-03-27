d0cf155bdbcb3debcdbe31a72ab4019f88329c36
JIMP85 <tharinol@gmail.com>
2023-03-27 11:18:46 +0200

    Validación NIF

---
+++ demo/src/test/java/com/example/DNI/ValidacionDNITest.java
@@ -0,0 +1,51 @@
+package com.example.DNI;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.hibernate.boot.jaxb.internal.CacheableFileXmlSource;
+import org.hibernate.grammars.hql.HqlParser.CaseOtherwiseContext;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import com.example.validacion.ValidacionNIF;
+
+
+public class ValidacionDNITest {
+	@Nested
+	class valido {
+		
+		@Nested
+		class OK {
+			
+			
+			@ParameterizedTest(name= "Caso: {0}")
+			@ValueSource(strings = {"12345678Z"})
+			void testValido(String caso){
+			assertTrue(ValidacionNIF.validarDNI(caso), "DNI correcto");
+			}
+		}
+		
+		@Nested
+		class KO {
+			
+			
+			@ParameterizedTest (name= "Caso: {0}")
+			@ValueSource(strings = {"00000000K","00000001R","99999999R","123445","R98234586","POIUYTMK9", " "})
+			@EmptySource
+			void validacionNegativa(String caso) {
+				
+				assertFalse(ValidacionNIF.validarDNI(caso), "no válidos");
+			
+		}
+						
+		
+		}
+	
+	}
+
+}
