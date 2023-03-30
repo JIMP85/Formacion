82590bcadbdd3d4db12236857113b8448b92d5fa
JIMP85 <tharinol@gmail.com>
2023-03-30 08:57:33 +0200

    Proyecto Catálogo

---
+++ Catalogo/src/main/java/com/example/domains/entities/Inventory.java
@@ -0,0 +1,60 @@
+package com.example.domains.entities;
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+
+/**
+ * The persistent class for the inventory database table.
+ * 
+ */
+@Entity
+@Table(name="inventory")
+@NamedQuery(name="Inventory.findAll", query="SELECT i FROM Inventory i")
+public class Inventory implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="inventory_id", unique=true, nullable=false)
+	private int inventoryId;
+
+	@Column(name="last_update", nullable=false)
+	private Timestamp lastUpdate;
+
+	//bi-directional many-to-one association to Film
+	@ManyToOne
+	@JoinColumn(name="film_id", nullable=false)
+	private Film film;
+
+
+	public Inventory() {
+	}
+
+	public int getInventoryId() {
+		return this.inventoryId;
+	}
+
+	public void setInventoryId(int inventoryId) {
+		this.inventoryId = inventoryId;
+	}
+
+	public Timestamp getLastUpdate() {
+		return this.lastUpdate;
+	}
+
+	public void setLastUpdate(Timestamp lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	public Film getFilm() {
+		return this.film;
+	}
+
+	public void setFilm(Film film) {
+		this.film = film;
+	}
+
+}
