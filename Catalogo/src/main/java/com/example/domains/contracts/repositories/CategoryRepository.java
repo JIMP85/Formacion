024be6e68666f330aecba72d285eede2c1ed075c
JIMP85 <tharinol@gmail.com>
2023-03-31 14:31:13 +0200

    Proyecto Catalogo Category novedades

--- Catalogo/src/main/java/com/example/domains/contracts/repositories/CategoryRepository.java
+++ Catalogo/src/main/java/com/example/domains/contracts/repositories/CategoryRepository.java
@@ -1,5 +1,6 @@
 package com.example.domains.contracts.repositories;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,6 @@ import com.example.domains.entities.Category;
 public interface CategoryRepository extends JpaRepository<Category, Integer>, JpaSpecificationExecutor<Category>, RepositoryWithProjections {
 
 	<T> List <T> findAllBy(Class<T> type);
+
+	List<Category> findByLastUpdateGreaterThanEqualOrderByLastUpdate(Timestamp fecha);
 }
--- Catalogo/src/main/java/com/example/domains/services/CategoryServiceImpl.java
+++ Catalogo/src/main/java/com/example/domains/services/CategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.domains.services;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
@@ -55,6 +56,11 @@ public class CategoryServiceImpl implements CategoryService{
 	public List<Category> getAll() {
 		
 		return dao.findAll();
+	}
+	
+	@Override
+	public List<Category> novedades(Timestamp fecha) {
+		return dao.findByLastUpdateGreaterThanEqualOrderByLastUpdate(fecha);
 	}
 	
 
