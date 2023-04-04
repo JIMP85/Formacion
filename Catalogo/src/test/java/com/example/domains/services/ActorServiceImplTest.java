a7b6d6042d30d17134e03bb1859c186e5aa793b1
JIMP85 <tharinol@gmail.com>
2023-04-04 19:27:02 +0200

    ActorServiceImplTest

--- Catalogo/src/test/java/com/example/domains/services/ActorServiceImplTest.java
+++ Catalogo/src/test/java/com/example/domains/services/ActorServiceImplTest.java
@@ -2,6 +2,9 @@ package com.example.domains.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -9,8 +12,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,6 +21,10 @@ import org.springframework.context.annotation.ComponentScan;
 
 import com.example.domains.contracts.repositories.ActorRepository;
 import com.example.domains.entities.Actor;
+import com.example.exceptions.DuplicateKeyException;
+import com.example.exceptions.InvalidDataException;
+
+
 
 @DataJpaTest
 @ComponentScan(basePackages = "com.example")
@@ -34,30 +41,67 @@ class ActorServiceImplTest {
 	void testGetAllNoVacia() {
 		List<Actor> lista= new ArrayList<>(
 				Arrays.asList(new Actor(1,"Dani", "Robira"),
-						new Actor(2,"Carmelo", "COTON"),
-						new Actor(3,"Capitan", "TAN")));
+						new Actor(2,"Angel", "Martin"),
+						new Actor(3,"Carlos", "Latre"),
+						new Actor(4, "Santiago", "Segura")));
 		when(dao.findAll()).thenReturn(lista);
 		var rslt = srv.getAll();
-		assertThat(rslt.size()).isEqualTo(3);
+		assertThat(rslt.size()).isEqualTo(4);
+		
 				
 	}
 	
 	@Test
-	void testGetOne_valid() {
+	void testGetOneValid() {
 		List<Actor> lista= new ArrayList<>(
-				Arrays.asList(new Actor(1,"Pepito", "GRILLO"),
-						new Actor(2,"Carmelo", "COTON"),
-						new Actor(3,"Capitan", "TAN")));
-		when(dao.findById(1)).thenReturn(Optional.of(new Actor(1,"Pepito", "GRILLO")));
+				Arrays.asList(new Actor(1,"Dani", "Robira"),
+						new Actor(2,"Angel", "Martin"),
+						new Actor(3,"Carlos", "Latre"),
+						new Actor(4, "Santiago", "Segura")));
+		when(dao.findById(1)).thenReturn(Optional.of(new Actor(3,"Carlos", "Latre")));
 		var rslt = srv.getOne(1);
-		assertThat(rslt.isPresent());
+		assertTrue(rslt.isPresent());
+		assertEquals(dao.findById(1), rslt);
 	}
 	
+	@Test
+	void testGetOne_notfound() {
+		when(dao.findById(1)).thenReturn(Optional.empty());
+		var rslt = srv.getOne(1);
+		assertTrue(rslt.isEmpty());
+	}
+	
+	@Test
+	@DisplayName("Actor añadido satisfactoriamente")
+	void testAñadirActorOk () throws DuplicateKeyException, InvalidDataException {
+		List<Actor> lista= new ArrayList<>(
+				Arrays.asList(new Actor(1,"Dani", "Robira"),
+						new Actor(2,"Angel", "Martin"),
+						new Actor(3,"Carlos", "Latre"),
+						new Actor(4, "Santiago", "Segura")));
+		
+		Actor actor = new Actor(5,"Florentino", "Fernandez");
+		srv.añadir(actor);
+		var rslt = srv.getOne(5);
+		assertNotNull(rslt);
+		assertEquals(dao.findById(5), rslt);
+		assertNotEquals(lista.get(3), rslt);
+		
+	}
+	
+	@Test
+	@DisplayName("Añadido incorrecto")
+	void testAdd() throws DuplicateKeyException, InvalidDataException {
+		when(dao.save(any(Actor.class))).thenReturn(null, null);
+		assertThrows(InvalidDataException.class, () -> srv.añadir(null));
+		verify(dao, times(0)).save(null);
+		
+	}
 	
 
-	@Test
-	void testDeleteById() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	void testDeleteById() {
+//		fail("Not yet implemented");
+//	}
 
 }
