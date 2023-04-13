fcc65f51465bdc4afd1b3e0a81c679c538149afc
JIMP85 <tharinol@gmail.com>
2023-04-13 15:03:35 +0200

    Prototipos

--- Fundamentos/demo.js
+++ Fundamentos/demo.js
@@ -87,3 +87,55 @@ t.filter(filtrado);
 
 
 coordenadas = (x,y) => x+y;
+
+
+//Dos formas de crear Arrays
+let tab = new Array(); 
+tab = [];
+
+tab = [10,20,30]; //En la mayoria de los lenguajes, los Arrays son fijos en la creación en cuanto a longitud
+tab[10]= "otro"; //En javaScript se puede modificar la longitud de los Arrays y no tienen que ser todos del mismo tipo
+tab[5]= (a,b) => a+b;
+tab.push('añade');
+tab.splice(3,1); //Elimina un elemento del índice 3
+
+console.log('Indices');
+for( v in tab){
+    console.log(v)
+};
+
+console.log('Valores');
+for (v of tab){
+    console.log(v)
+};
+//console.log(tab[5](2,3)) //Le pasa los parámetros 2 y 3 a la función que hay en el Array en la posición 5
+
+let o = new Object();
+o={};
+
+o.nombre = 'Pepito';
+o['apellidos'] = 'Grillo',
+o.nom = ()=> this.nombre+ ' '+this.apellidos;
+p = {nombre : 'carmelo', apellidos: 'coton', nom: ()=> this.nombre + ' ' + this.apellidos};
+o=p;
+
+console.log(`${o.nombre} ${o.apellidos} ${o.nom()}`);
+
+function MiClase(elId, elNombre) {
+    this.id = elId;
+    this.nombre = elNombre;
+    this.muestraId = function() {
+    alert("El ID del objeto es " + this.id);
+    }
+    this.ponNombre = function(nom) {
+    this.nombre=nom.toUpperCase();
+    }
+}
+    var elObjeto = new MiClase("99", "Objeto de prueba");
+    var otroObjeto = MiClase("99", "Objeto de prueba"); //No es función constructora (no tiene el new), por lo tanto devuelve undefinned
+                                                        // si no tiene return
+
+ MiClase.prototype.cotilla = () => console.log('estoy en el prototype')
+ var o1 =  new Miclase ("77", "Objeto de prueba");
+ o1.cotilla();
+
