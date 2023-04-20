adb72c9df44f4357fca7aafe2efde208799f7def
JIMP85 <tharinol@gmail.com>
2023-04-20 20:04:48 +0200

    Actores Front

--- demo-react/src/comunes.js
+++ demo-react/src/comunes.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import imgAjaxLoading from './imagenes/loading.gif'
+
 
 export class ErrorBoundary extends React.Component {
     constructor(props) {
@@ -21,4 +23,63 @@ export class ErrorBoundary extends React.Component {
         }
         return this.props.children;
     }
+}
+
+export function Esperando() {
+    return <div>
+    <div className="ajax-wait"></div>
+    <div className="loader"></div>
+    {/* <img className="ajax-wait" src={loadingImage} alt="Cargando ..." /> */}
+    </div>;
+}
+
+export function ErrorMessage({ msg, onClear }) {
+    if (msg) {
+    return (
+    <div
+    className="alert alert-danger alert-dismissible fade show"
+    role="alert"
+    >
+    {msg}
+    <button
+    type="button"
+    className="btn-close"
+    data-dismiss="alert"
+    aria-label="Close"
+    onClick={e => onClear && onClear()}
+    />
+    </div>
+    );
+    }
+    return null;
+}
+
+export function PaginacionCmd({ actual, total, onChange }) {
+    let click = (number, ev) => {
+        ev.preventDefault()
+        if (onChange) onChange(number)
+    }
+    let items = [];
+    for (let number = 0; number < total; number++) {
+        items.push(
+            number === actual ?
+                <li key={number} className="page-item active" aria-current="page"><a href='.' className="page-link" onClick={click.bind(this, number)} >{number + 1}</a></li>
+                :
+                <li key={number} className="page-item"><a href='.' className="page-link" onClick={click.bind(this, number)} >{number + 1}</a></li>
+        );
+    }
+    return (
+        <nav aria-label="Page navigation">
+            <ul className="pagination">
+                {items}
+            </ul>
+        </nav>
+    )
+}
+
+export function ValidationMessage({ msg }) {
+    if (msg) {
+    return <output className="errorMsg">{msg}</output>;
+    }
+    return null;
 }
